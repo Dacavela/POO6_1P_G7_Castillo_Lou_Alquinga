41a4bc055a970f8140f7f41c5847b8c6bff2e420
David Castillo <Dacavela@espol.edu.ec>
2021-11-17 17:21:11 -0500

    Nuevo repositorio con nombre correcto

---
+++ Proyecto1P/src/main/java/com/mycompany/proyecto1p/Conductor.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.proyecto1p;
+
+/**
+ *
+ * @author Davca
+ */
+public class Conductor {
+    private String licencia;
+    private char estado;
+    private int vehiculo;
+    
+    
+    
+    
+}
---
+++ Proyecto1P/src/main/java/com/mycompany/proyecto1p/Usuario.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.proyecto1p;
+
+/**
+ *
+ * @author Davca
+ */
+public class Usuario {
+    protected String cedula;
+    protected String nombres;
+    protected String apellidos;
+    protected int edad;
+    protected String user;
+    protected String password;
+    protected String celular;
+    
+    
+}
